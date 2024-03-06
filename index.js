5f39bbba13307b23bb814fd771680e009fe4b6b6
Aaron Lin <aaron.l.lin.general@gmail.com>
2024-03-06 16:15:20 -0500

    attempt1

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+let cats = ["Milo","Otis","Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    let catz = [...cats,name];
+    return catz
+}
+function prependCat(name){
+    let catz = [name,...cats];
+    return catz
+}
+function removeLastCat(){
+    let catz = cats.slice(0, cats.length-1);
+    return catz
+}
+function removeFirstCat(){
+    let catz = cats.slice(1);
+    return catz
+}
